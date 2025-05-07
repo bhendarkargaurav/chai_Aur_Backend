59039e8c2f1295dd1b5ac26ecf2ca7a19220104b
Gaurav <gaurav.bhe.24@gmail.com>
2025-05-07 17:23:57 +0530

    add user and video model

---
+++ src/models/user.models.js
@@ -0,0 +1,92 @@
+import mongoose from "mongoose"
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
+
+const UserSchema = new mongoose.Schema({
+
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        index: true     // for searching its imp
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+    },
+    fullname: {
+        type: String,
+        required: true,
+        trim: true,
+        index: true
+    },
+    avatar: {
+        type: String,  // cloudnary url
+        required: true,
+    },
+    coverimage: {
+        type: String   // cloudnary url
+    },
+    watchHistory: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "video"
+        }
+    ],
+    password: {
+        type: String,
+        required: [true, 'Password is required']
+    },
+    refreshToken: {
+        type: String
+    },
+    
+}, {timestamps: true});
+
+// hooks we can use this to perfor something before saving the dat in the code
+// like incrypt the password (in salt format)and all
+
+UserSchema.pre("save", async function (next){
+    if(!this.isModified("password")) return next();
+    this.password = bcrypt.hash(this.password, 10)
+    next()
+})
+
+// capare the pass it give the result in True and false formate
+UserSchema.methods.isPasswordCorrect = async function(password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+UserSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        { // payload
+            _id: this._id, // from mongodb
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+UserSchema.methods.generateRefreshToken = function(){
+    // same as access token
+    return jwt.sign(
+        { // payload
+            _id: this._id, // from mongodb
+            email: this.email,
+            username: this.username,
+            fullname: this.fullname
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+export const User = mongoose.model('User', UserSchema);
