d3f9c8f84bb669f2d290c22646b3593041aebb31
Gaurav <gaurav.bhe.24@gmail.com>
2025-05-23 17:49:20 +0530

    add user routes to manage accound

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,7 +3,14 @@ import {
     loginUser, 
     LogOutUser, 
     registerUser, 
-    refreshAccssToken 
+    refreshAccssToken, 
+    changeCurrentPassword,
+    getCurrentUser,
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverImage,
+    getUserChannelProfile,
+    getWatchHistory
     } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -26,6 +33,21 @@ router.route("/login").post(loginUser)
 router.route("/logout").post(verifyJWT, LogOutUser)      // verifyJWT from auth middleware
 
 router.route("/refresh-Token").post(refreshAccssToken)
+
+router.route("/changepassword").post(verifyJWT, changeCurrentPassword)
+
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+
+router.route("/update-accound").patch(verifyJWT, updateAccountDetails)
+
+router.route("/update-avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+
+router.route("/update-coverimage").patch(verifyJWT, upload.single("/coverimage"), updateUserCoverImage)
+
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+
+router.route("/watchhistory").get(verifyJWT, getWatchHistory)
+
 export default router
 
 
